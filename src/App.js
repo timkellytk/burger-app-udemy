fc5da6844e522bda5fb024a5a1a8385e91893be9
Tim Kelly <timpkelly1@gmail.com>
2020-04-28 17:28:47 +1000

    Moved the layout component to a hoc folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './containers/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
