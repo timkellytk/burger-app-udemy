b8eaa296bad7056418d0005b2f6a2d0b2a3025be
Tim Kelly <timpkelly1@gmail.com>
2020-05-26 15:28:31 +1000

    Updating the updatePurchaseState()

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,8 +11,6 @@ import * as ActiveType from '../../store/action';
 
 class BurgerBuilder extends Component {
   state = {
-    totalPrice: 5,
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -39,9 +37,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({
-      purchasable: sum > 0,
-    });
+    return sum > 0;
   };
 
   purchaseHandler = () => {
@@ -122,7 +118,7 @@ class BurgerBuilder extends Component {
             removedIngredient={this.props.onRemoveIngredient}
             disabled={disabledIngredients}
             price={this.props.price}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             purchased={this.purchaseHandler}
           />
         </React.Fragment>
@@ -164,11 +160,10 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onAddIngredient: (ingName, updatePurchaseState) =>
+    onAddIngredient: (ingName) =>
       dispatch({
         type: ActiveType.ADD_INGREDIENT,
         ingredientName: ingName,
-        updatePurchaseState: updatePurchaseState,
       }),
     onRemoveIngredient: (ingName) =>
       dispatch({ type: ActiveType.REMOVE_INGREDIENT, ingredientName: ingName }),
