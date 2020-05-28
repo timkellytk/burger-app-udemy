a35197d5d20fce31966d8a12f7b71cd5d95ae586
Tim Kelly <timpkelly1@gmail.com>
2020-05-29 09:39:24 +1000

    Adding order actions to redux - have not connected data container or actions yet

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -102,15 +102,6 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: orderForm,
     };
-    axios
-      .post('/orders.json', order)
-      .then((response) => {
-        this.setState({ loading: false, purchasing: false });
-        this.props.history.push('/');
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
   };
 
   checkValidity = (value, rule) => {
---
+++ src/store/actions/order.js
@@ -0,0 +1,30 @@
+import * as actionType from './actionType';
+import axios from '../../axios-orders';
+
+const purchaseBurgerSuccess = (id, orderData) => {
+  return {
+    type: actionType.PURCHASE_BURGER_SUCCESS,
+    orderId: id,
+    orderData: orderData,
+  };
+};
+
+const purchaseBurgerFail = (error) => {
+  return {
+    type: actionType.PURCHASE_BURGER_FAIL,
+    error: error,
+  };
+};
+
+export const purchaseBurgerStart = (orderData) => {
+  return (dispatch) => {
+    axios
+      .post('/orders.json', orderData)
+      .then((response) => {
+        dispatch(purchaseBurgerSuccess(response.data, orderData));
+      })
+      .catch((error) => {
+        dispatch(purchaseBurgerFail(error));
+      });
+  };
+};
