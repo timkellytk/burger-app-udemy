d2ce54d7176c5f0902493e2c90fc78de11c20d32
Tim Kelly <timpkelly1@gmail.com>
2020-05-30 11:47:25 +1000

    Succesfully set up firebase to fire an axios post request that creates a new users and gets a token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionType';
+import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -23,5 +24,24 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return (dispatch) => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    console.log(authData);
+    axios
+      .post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDmE7CvW8STa2tIaDIqiJqYO_nT5HflPds',
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((err) => {
+        console.log(err.response);
+        dispatch(authFail(err));
+      });
   };
 };
