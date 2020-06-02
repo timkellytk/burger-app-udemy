d7d052cca75a3ad9a49a968a3b2c6b2c83b7d929
Tim Kelly <timpkelly1@gmail.com>
2020-06-02 13:23:11 +1000

    Updates

---
+++ precache-manifest.ecbab1a01d481a2ee7c646f06e012187.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc984e67d84aa0e0a25365eba335070c",
+    "url": "/burger-app-udemy/index.html"
+  },
+  {
+    "revision": "d9b5540cfba2611d7403",
+    "url": "/burger-app-udemy/static/css/3.8187a38a.chunk.css"
+  },
+  {
+    "revision": "de0f2d916332a5db6c10",
+    "url": "/burger-app-udemy/static/css/4.e61d535d.chunk.css"
+  },
+  {
+    "revision": "00b1543a3ad58124f089",
+    "url": "/burger-app-udemy/static/css/5.b4bed6bd.chunk.css"
+  },
+  {
+    "revision": "8cc571101257cbbb4900",
+    "url": "/burger-app-udemy/static/css/main.dec72cbb.chunk.css"
+  },
+  {
+    "revision": "af4508c38d40c6f2c12d",
+    "url": "/burger-app-udemy/static/js/2.e54c7b81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-app-udemy/static/js/2.e54c7b81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b5540cfba2611d7403",
+    "url": "/burger-app-udemy/static/js/3.f31b9d4b.chunk.js"
+  },
+  {
+    "revision": "de0f2d916332a5db6c10",
+    "url": "/burger-app-udemy/static/js/4.b7d671ab.chunk.js"
+  },
+  {
+    "revision": "00b1543a3ad58124f089",
+    "url": "/burger-app-udemy/static/js/5.db45b1c8.chunk.js"
+  },
+  {
+    "revision": "8cc571101257cbbb4900",
+    "url": "/burger-app-udemy/static/js/main.6653f75b.chunk.js"
+  },
+  {
+    "revision": "451c9574d68757ce4cc9",
+    "url": "/burger-app-udemy/static/js/runtime-main.869f8892.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app-udemy/static/media/original.b8503d26.png"
+  }
+]);
